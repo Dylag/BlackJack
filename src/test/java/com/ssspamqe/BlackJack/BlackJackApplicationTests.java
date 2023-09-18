ead4308960bf0f34390fe49129c5e42d2bfc820d
ssspamqe <80048504+Dylag@users.noreply.github.com>
2023-09-18 16:53:22 +0300

    Initial commit

---
+++ src/test/java/com/ssspamqe/BlackJack/BlackJackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssspamqe.BlackJack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackJackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
